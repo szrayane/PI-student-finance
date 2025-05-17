d519e6536b63d432c99860e1a1eea52b727b9ea9
BpCampos <campos.bruno98@gmail.com>
2025-05-17 16:04:27 -0300

    fazendo a pagina de login ser a principal

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
